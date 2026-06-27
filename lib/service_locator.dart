9c0cd3c072375d6ff22d10d90b4ded8d7ccd7c47
hassanolaa <hssanabdl975@gmail.com>
2026-06-27 23:56:09 +0300

    Add the db to app assets, make the path dynamically

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'features/games/data/datasources/flatpak_data_source.dart';
@@ -7,14 +10,31 @@ import 'features/games/presentation/cubit/games_cubit.dart';
 import 'features/categories/data/datasources/categories_local_data_source.dart';
 import 'features/categories/data/repositories/categories_repository_impl.dart';
 import 'features/categories/presentation/cubit/categories_cubit.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 
 final sl = GetIt.instance;
 
 Future<void> initServiceLocator() async {
-  // Database
+  // Initialize SQLite
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
-  final database = await databaseFactory.openDatabase('/home/hassanola/Desktop/converter/store.db');
+
+  // Get the application support directory
+  final appDir = await getApplicationSupportDirectory();
+  final dbPath = p.join(appDir.path, 'store.db');
+
+  // Copy the bundled database on first run
+  if (!await File(dbPath).exists()) {
+    await Directory(appDir.path).create(recursive: true);
+
+    final data = await rootBundle.load('assets/db/store.db');
+
+    await File(dbPath).writeAsBytes(data.buffer.asUint8List(), flush: true);
+  }
+
+  // Open the copied database
+  final database = await databaseFactory.openDatabase(dbPath);
 
   // Core
   sl.registerLazySingleton<Database>(() => database);
@@ -23,24 +43,17 @@ Future<void> initServiceLocator() async {
   sl.registerLazySingleton<GamesLocalDataSource>(
     () => GamesLocalDataSourceImpl(database: sl()),
   );
-  sl.registerLazySingleton<FlatpakDataSource>(
-    () => FlatpakDataSourceImpl(),
-  );
+  sl.registerLazySingleton<FlatpakDataSource>(() => FlatpakDataSourceImpl());
   sl.registerLazySingleton<CategoriesLocalDataSource>(
     () => CategoriesLocalDataSourceImpl(database: sl()),
   );
 
   // Repositories
   sl.registerLazySingleton<GamesRepositoryImpl>(
-    () => GamesRepositoryImpl(
-      localDataSource: sl(),
-      flatpakDataSource: sl(),
-    ),
+    () => GamesRepositoryImpl(localDataSource: sl(), flatpakDataSource: sl()),
   );
   sl.registerLazySingleton<CategoriesRepositoryImpl>(
-    () => CategoriesRepositoryImpl(
-      localDataSource: sl(),
-    ),
+    () => CategoriesRepositoryImpl(localDataSource: sl()),
   );
 
   // Cubits
